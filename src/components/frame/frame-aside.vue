5821450db2c27d11185fb8d867ce705213e1a0ad
yogurtq <631551358@qq.com>
2021-05-14 18:19:30 +0800

    fix:update component's name

--- src/components/frame/frame-aside.vue
+++ src/components/frame/frame-aside.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'frame-aside',
+  name: 'frameAside',
   data() {
     return {};
   },
--- src/components/frame/frame-header.vue
+++ src/components/frame/frame-header.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'frame-header',
+  name: 'frameHeader',
   data() {
     return {};
   },
