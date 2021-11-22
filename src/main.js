e55fbaea4ce0c391cb5d23169b0e3fc2b7a31b59
YornQiu <631551358@qq.com>
2021-11-22 14:42:06 +0800

    feat:service worker

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
 
 import '@/styles/index.scss';
 
--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "Home" */ './views/Home.vue')
   }
 ];
 
